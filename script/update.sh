0a1f8aea1291a4ea9f1552851fc536b0e8bd0155
Xlous <35778015+ixmu@users.noreply.github.com>
2023-11-12 22:43:20 +0800

    Update update.sh

--- script/update.sh
+++ script/update.sh
@@ -36,7 +36,9 @@ google_china="$(curl -kLfsm 5 https://raw.githubusercontent.com/felixonmars/dnsm
 cdn_testlist="$(curl -kLfsm 5 https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/cdn-testlist.txt)"
 cust_cndomain="$(cat script/cust_cndomain.conf)"
 domain_list="$accelerated_domains\n$apple_china\n$google_china\n$cdn_testlist\n$cust_cndomain"
-echo -e "${domain_list}" | sort | uniq |sed -e 's/#.*//g' -e '/^$/d' -e 's/server=\///g' -e 's/\/114.114.114.114//g' -e 's/^/nameserver \/\./g' -e 's/$/\/private/' | sort -u >direct-list.conf
+echo -e "${domain_list}" | sort | uniq |sed -e 's/#.*//g' -e '/^$/d' -e 's/server=\///g' -e 's/\/114.114.114.114//g' | sort -u >direct-list.conf
 
 # Update Proxy Domain List
 cat /tmp/temp_gfwlist | sed -e '/^$/d' -e 's/^/\./' > proxy-domain-list.conf
+
+# China Domain
