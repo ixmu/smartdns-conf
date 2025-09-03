1cd2748181e5949a9cee3bbba1f0b10e1b670852
marx peng <pengxp1996@hotmail.com>
2025-09-03 22:57:05 +0800

    up

--- script/update.sh
+++ script/update.sh
@@ -4,22 +4,11 @@ curl -sS https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt | 
     sed '/apple\.com/d; /sina\.cn/d; /sina\.com\.cn/d; /baidu\.com/d; /qq\.com/d' | \
     sed '/^[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+$/d' | grep '^[0-9a-zA-Z\.-]\+$' | \
     grep '\.' | sed 's#^\.\+##' | sort -u > /tmp/temp_gfwlist1
-
 curl -sS https://raw.githubusercontent.com/hq450/fancyss/master/rules/gfwlist.conf | \
     sed 's/ipset=\/\.//g; s/\/gfwlist//g; /^server/d' > /tmp/temp_gfwlist2
-
 curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/gfw.txt > /tmp/temp_gfwlist3
-
-cat /tmp/temp_gfwlist1 /tmp/temp_gfwlist2 /tmp/temp_gfwlist3 script/cust_gfwdomain.conf | \
-    sort -u | sed 's/^\.*//g' > /tmp/temp_gfwlist
-cat /tmp/temp_gfwlist | sed -e '/^$/d' > proxy-domain-list.conf
-
-# Update China IPV4 List
-qqwry="$(curl -kLfsm 5 https://raw.githubusercontent.com/metowolf/iplist/master/data/special/china.txt)"
-ipipnet="$(curl -kLfsm 5 https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt)"
-clang="$(curl -kLfsm 5 https://ispip.clang.cn/all_cn.txt)"
-iplist="$qqwry\n$ipipnet\n$clang"
-echo -e "${iplist}" | sort | uniq |sed -e '/^$/d' -e 's/^/blacklist-ip /g' >blacklist-ip.conf
+cat /tmp/temp_gfwlist1 /tmp/temp_gfwlist2 /tmp/temp_gfwlist3  | \
+    sort -u | sed 's/^\.*//g' > /tmp/temp_gfwlist | sed -e '/^$/d' > proxy-domain-list.conf
 
 # Update China List
 accelerated_domains="$(curl -kLfsm 5 https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf)"
